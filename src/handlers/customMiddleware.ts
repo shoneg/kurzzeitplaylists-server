fc31ff4e4485a6c097f9484f5c89d02e7c6bf9fd
Simon Huesch <simonhuesch@posteo.de>
2022-10-23 19:12:58 +0200

    ups

--- src/handlers/customMiddleware.ts
+++ src/handlers/customMiddleware.ts
@@ -5,9 +5,6 @@ import { MyUser } from '../types';
 export const ensureAuthenticated: RequestHandler = (req, res, next) => {
   if (req.isAuthenticated()) {
     spotify.setAccessToken((req.user as MyUser)?.accessToken);
-    if ((5 < 4 && 'test') || (2 + 6 < 5 && 5 < 4 && 'test') || 2 + 6 < 5 || (5 < 4 && 'test') || 2 + 6 < 5) {
-      console.log();
-    }
     return next();
   }
   res.redirect('/');
