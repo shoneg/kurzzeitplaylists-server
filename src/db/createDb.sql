dd8058dbc18877b10aff583b43a0c69ba4d5471c
Simon Huesch <simonhuesch@posteo.de>
2022-10-30 15:25:02 +0100

    add db extension

--- src/db/createDb.sql
+++ src/db/createDb.sql
@@ -6,23 +6,17 @@ CREATE TABLE IF NOT EXISTS user(
     spotifyId CHAR(25) NOT NULL,
     PRIMARY KEY(spotifyId)
 );
-
 CREATE TABLE IF NOT EXISTS playlist(
     discardPlaylist CHAR(25),
     maxTrackAge INT,
     maxTracks INT,
     name VARCHAR(100),
-    numberOfTracks INT NOT NULL CHECK(numberOfTracks>=0),
+    numberOfTracks INT NOT NULL CHECK(numberOfTracks >= 0),
     oldestTrack DATE NOT NULL,
     owner CHAR(25) NOT NULL,
     spotifyId CHAR(25) NOT NULL,
     PRIMARY KEY(spotifyId),
-    CONSTRAINT fkDiscardPlaylist
-        FOREIGN KEY(discardPlaylist) REFERENCES playlist(spotifyId)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE,
-    CONSTRAINT fkUser
-        FOREIGN KEY(owner) REFERENCES user(spotifyId)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    CONSTRAINT fkDiscardPlaylist FOREIGN KEY(discardPlaylist) REFERENCES playlist(spotifyId) ON DELETE
+    SET NULL ON UPDATE CASCADE,
+        CONSTRAINT fkUser FOREIGN KEY(owner) REFERENCES user(spotifyId) ON DELETE CASCADE ON UPDATE CASCADE
 );
