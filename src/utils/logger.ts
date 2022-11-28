d05306cad1e98dffbd48b72d4896db1cbc50b428
Simon Huesch <simonhuesch@posteo.de>
2022-11-28 16:10:51 +0100

    edit logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -23,28 +23,46 @@ export default class Logger {
 
   public log(...messages: any[]) {
     if (this.debug == 0) {
-      const newMessages = this.messageMapper(messages);
-      console.log(this.tag, ...newMessages);
+      // const newMessages = this.messageMapper(messages);
+      const newMessages = messages;
+      console.log.apply(console, [this.tag, ...newMessages]);
     }
   }
   public info(...messages: any[]) {
     if (this.debug <= 1) {
-      const newMessages = this.messageMapper(messages);
-      console.info(this.tag, ...newMessages);
+      // const newMessages = this.messageMapper(messages);
+      const newMessages = messages;
+      console.info.apply(console, [this.tag, ...newMessages]);
     }
   }
   public warn(...messages: any[]) {
     if (this.debug <= 2) {
-      const newMessages = this.messageMapper(messages);
-      console.warn(this.tag, ...newMessages);
+      // const newMessages = this.messageMapper(messages);
+      const newMessages = messages;
+      console.warn.apply(console, [this.tag, ...newMessages]);
     }
   }
   public error(...messages: any[]) {
-    const newMessages = this.messageMapper(messages);
-    console.error(this.tag, ...newMessages);
+    // const newMessages = this.messageMapper(messages);
+    const newMessages = messages;
+    console.error.apply(console, [this.tag, ...newMessages]);
   }
   public forceLog(...messages: any[]) {
-    const newMessages = this.messageMapper(messages);
-    console.log(this.tag, ...newMessages);
+    // const newMessages = this.messageMapper(messages);
+    const newMessages = messages;
+    console.log.apply(console, [this.tag, ...newMessages]);
+  }
+  public assert(condition: boolean | (() => boolean), ...messages: any[]) {
+    let _cond = true;
+    if (typeof condition === 'boolean') {
+      _cond = condition;
+    } else {
+      _cond = condition();
+    }
+    if (!_cond) {
+      // const newMessages = this.messageMapper(messages);
+      const newMessages = messages;
+      console.log.apply(console, [this.tag, 'Assertion failed:', ...newMessages]);
+    }
   }
 }
